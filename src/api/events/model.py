dea38a081f689173cd6552baacf2451e9efd0bac
Dipen Kumar <dipenbachani@gmail.com>
2025-05-13 21:27:26 -0700

    Add TimescaleDB support and timezone config

--- src/api/events/model.py
+++ src/api/events/model.py
@@ -3,26 +3,33 @@ from sqlmodel import SQLModel, Field
 from typing import Optional, List
 from datetime import datetime, timezone
 import sqlmodel
+from timescaledb import TimescaleModel
+from timescaledb.utils import get_utc_now
 
 
-def get_utc_now():
-    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
+# def get_utc_now():
+#     return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
+
+# page visits at any give time
 
 
-class EventModel(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    page: Optional[str] = ""
+class EventModel(TimescaleModel, table=True):
+    # id: Optional[int] = Field(default=None, primary_key=True)
+    page: str = Field(index=True)  # /about, /contact etc
     description: Optional[str] = ""
-    created_at: datetime = Field(
-        default_factory=get_utc_now,
-        sa_type=sqlmodel.DateTime(timezone=True),
-        nullable=False,
-    )
+    # created_at: datetime = Field(
+    #     default_factory=get_utc_now,
+    #     sa_type=sqlmodel.DateTime(timezone=True),
+    #     nullable=False,
+    # )
     updated_at: datetime = Field(
         default_factory=get_utc_now,
         sa_type=sqlmodel.DateTime(timezone=True),
         nullable=False,
     )
+
+    __chunk_time_interval__ = "INTERVAL 1 day"
+    __drop_after__ = "INTERVAL 3 months"
 
 
 class EventCreateSchema(SQLModel):
